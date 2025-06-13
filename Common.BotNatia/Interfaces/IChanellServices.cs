6ce9de4aa58ce83b70862034c03e3c197b0b4c8e
Guga's PC <Guga's PC@DESKTOP-2DB9EPL>
2025-06-13 13:16:04 -0700

    add voice support

--- Common.BotNatia/Interfaces/IChanellServices.cs
+++ Common.BotNatia/Interfaces/IChanellServices.cs
@@ -4,4 +4,6 @@ public interface IChanellServices
 {
     Task<List<int>> GetPortsWhereAlarmsIsOn();
     Task<List<string>> GetChannelsByPortIn250ListAsync(List<int> portIds);
+    Task<Stream> ConvertWavToOggStreamAsync(byte[] wavData);
+    Task<byte[]> SpeakNow(string text, int baseRate = 2);
 }
